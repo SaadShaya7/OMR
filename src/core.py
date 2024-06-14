384259ca872e04103ae063c4ff323165a352c237
Saad Shayah <saad.shayah@gmail.com>
2024-06-14 20:08:44 +0300

    refactor: better comments and naming conventions in read_omr_response function

--- src/core.py
+++ src/core.py
@@ -132,7 +132,6 @@ class ImageInstanceOps:
 
         try:
             img = image.copy()
-
             img = cv2.flip(img, 1)
             img = ImageUtils.resize_util(
                 img, template.page_dimensions[0], template.page_dimensions[1]
@@ -149,133 +148,146 @@ class ImageInstanceOps:
             if auto_align:
                 template.field_blocks = self.apply_auto_align(config, template, morph)
 
-            all_q_vals, all_q_strip_arrs, all_q_std_vals = [], [], []
-            total_q_strip_no = 0
+            # Initialize lists to store all average values, all strip values arrays, and all standard deviation values
+            all_mean_values = []
+            all_strip_values_arrays = []
+            all_standard_deviation_values = []
+            total_strip_count = 0
+
+            # Iterate through each field block in the template
             for field_block in template.field_blocks:
-                box_w, box_h = field_block.bubble_dimensions
-                q_std_vals = []
-                for field_block_bubbles in field_block.traverse_bubbles:
-                    q_strip_vals = []
-                    for pt in field_block_bubbles:
+                bubble_width, bubble_height = field_block.bubble_dimensions
+                field_standard_deviation_values = []
 
-                        x, y = (pt.x + field_block.shift, pt.y)
-                        rect = [y, y + box_h, x, x + box_w]
-                        q_strip_vals.append(
-                            cv2.mean(img[rect[0] : rect[1], rect[2] : rect[3]])[0]
-                        )
-                    q_std_vals.append(round(np.std(q_strip_vals), 2))
-                    all_q_strip_arrs.append(q_strip_vals)
+                # Traverse through bubbles in the field block
+                for bubble_group in field_block.traverse_bubbles:
+                    strip_values = []
 
-                    all_q_vals.extend(q_strip_vals)
+                    # Calculate mean values for each bubble point
+                    for bubble_point in bubble_group:
+                        x = bubble_point.x + field_block.shift
+                        y = bubble_point.y
+                        rect = [y, y + bubble_height, x, x + bubble_width]
+                        mean_value = cv2.mean(
+                            img[rect[0] : rect[1], rect[2] : rect[3]]
+                        )[0]
+                        strip_values.append(mean_value)
 
-                    total_q_strip_no += 1
-                all_q_std_vals.extend(q_std_vals)
+                    # Calculate the standard deviation of the strip values and round to 2 decimal places
+                    field_standard_deviation_values.append(
+                        round(np.std(strip_values), 2)
+                    )
 
-            global_std_thresh, _, _ = self.get_global_threshold(all_q_std_vals)
+                    # Append the strip values to the overall arrays
+                    all_strip_values_arrays.append(strip_values)
+                    all_mean_values.extend(strip_values)
+                    total_strip_count += 1
 
-            global_thr, _, _ = self.get_global_threshold(all_q_vals, looseness=4)
+                # Extend the overall standard deviation values with the field's standard deviation values
+                all_standard_deviation_values.extend(field_standard_deviation_values)
+
+            global_std_thresh, _, _ = self.get_global_threshold(
+                all_standard_deviation_values
+            )
+
+            global_thr, _, _ = self.get_global_threshold(all_mean_values, looseness=4)
 
             logger.info(
                 f"Thresholding:\tglobal_thr: {round(global_thr, 2)} \tglobal_std_THR: {round(global_std_thresh, 2)}\t{'(Looks like a Xeroxed OMR)' if (global_thr == 255) else ''}"
             )
+            # Initialize variables for OMR threshold average, total strip count, and total box count
+            omr_threshold_avg = 0
+            total_strip_count = 0
+            total_box_count = 0
 
-            per_omr_threshold_avg, total_q_strip_no, total_q_box_no = 0, 0, 0
+            # Convert the final marked image to BGR color space
             final_marked = cv2.cvtColor(final_marked, cv2.COLOR_GRAY2BGR)
 
+            # Iterate through each field block in the template
             for field_block in template.field_blocks:
-                block_q_strip_no = 1
-                box_w, box_h = field_block.bubble_dimensions
-                shift = field_block.shift
-                s, d = field_block.origin, field_block.dimensions
-                key = field_block.name[:3]
+                block_strip_count = 1
+                bubble_width, bubble_height = field_block.bubble_dimensions
+                block_key = field_block.name[:3]
 
                 question_index = 0
-                for field_block_bubbles in field_block.traverse_bubbles:
+                for bubble_group in field_block.traverse_bubbles:
 
-                    no_outliers = all_q_std_vals[total_q_strip_no] < global_std_thresh
+                    # Determine if there are no outliers based on standard deviation
+                    no_outliers = (
+                        all_standard_deviation_values[total_strip_count]
+                        < global_std_thresh
+                    )
 
-                    per_q_strip_threshold = self.get_local_threshold(
-                        all_q_strip_arrs[total_q_strip_no],
+                    # Get the local threshold for the current strip of bubbles
+                    strip_threshold = self.get_local_threshold(
+                        all_strip_values_arrays[total_strip_count],
                         global_thr,
                         no_outliers,
-                        f"Mean Intensity Histogram for {key}.{field_block_bubbles[0].field_label}.{block_q_strip_no}",
+                        f"Mean Intensity Histogram for {block_key}.{bubble_group[0].field_label}.{block_strip_count}",
                         config.outputs.show_image_level >= 6,
                     )
 
-                    per_omr_threshold_avg += per_q_strip_threshold
+                    omr_threshold_avg += strip_threshold
 
                     detected_bubbles = []
-                    for bubble in field_block_bubbles:
-                        bubble_is_marked = (
-                            per_q_strip_threshold > all_q_vals[total_q_box_no]
+                    for bubble in bubble_group:
+                        is_bubble_marked = (
+                            strip_threshold > all_mean_values[total_box_count]
                         )
-                        total_q_box_no += 1
+                        total_box_count += 1
 
-                        if bubble_is_marked:
+                        if is_bubble_marked:
                             detected_bubbles.append(bubble)
-                            x, y, field_value = (
-                                bubble.x + field_block.shift,
-                                bubble.y,
-                                bubble.field_value,
-                            )
+                            x = bubble.x + field_block.shift
+                            y = bubble.y
 
-                            if field_block.correct_answers == None:
-                                cv2.rectangle(
-                                    final_marked,
-                                    (int(x + box_w / 12), int(y + box_h / 12)),
-                                    (
-                                        int(x + box_w - box_w / 12),
-                                        int(y + box_h - box_h / 12),
-                                    ),
-                                    (0, 0, 255),
-                                    2,
+                            # Draw rectangles based on marking status
+                            if field_block.correct_answers is None:
+                                color = (
+                                    0,
+                                    0,
+                                    255,
                                 )
-
                             elif (
                                 field_block.correct_answers[question_index]
                                 == bubble.field_value
                             ):
-
-                                cv2.rectangle(
-                                    final_marked,
-                                    (int(x + box_w / 12), int(y + box_h / 12)),
-                                    (
-                                        int(x + box_w - box_w / 12),
-                                        int(y + box_h - box_h / 12),
-                                    ),
-                                    (0, 255, 0),
-                                    2,
-                                )
+                                color = (0, 255, 0)
                             else:
-                                cv2.rectangle(
-                                    final_marked,
-                                    (int(x + box_w / 12), int(y + box_h / 12)),
-                                    (
-                                        int(x + box_w - box_w / 12),
-                                        int(y + box_h - box_h / 12),
-                                    ),
-                                    (255, 0, 0),
-                                    2,
-                                )
+                                color = (255, 0, 0)
 
+                            cv2.rectangle(
+                                final_marked,
+                                (
+                                    int(x + bubble_width / 12),
+                                    int(y + bubble_height / 12),
+                                ),
+                                (
+                                    int(x + bubble_width - bubble_width / 12),
+                                    int(y + bubble_height - bubble_height / 12),
+                                ),
+                                color,
+                                2,
+                            )
+
+                    # Record detected bubbles in the OMR response
                     for bubble in detected_bubbles:
-                        field_label, field_value = (
-                            bubble.field_label,
-                            bubble.field_value,
-                        )
-
+                        field_label = bubble.field_label
+                        field_value = bubble.field_value
                         omr_response[field_label] = field_value
 
+                    # Handle case where no bubbles are detected
                     if len(detected_bubbles) == 0:
-                        field_label = field_block_bubbles[0].field_label
+                        field_label = bubble_group[0].field_label
                         omr_response[field_label] = field_block.empty_val
 
-                    block_q_strip_no += 1
-                    total_q_strip_no += 1
+                    block_strip_count += 1
+                    total_strip_count += 1
                     question_index += 1
 
-            per_omr_threshold_avg /= total_q_strip_no
-            per_omr_threshold_avg = round(per_omr_threshold_avg, 2)
+            # Calculate the average OMR threshold and flip the final marked image
+            omr_threshold_avg /= total_strip_count
+            omr_threshold_avg = round(omr_threshold_avg, 2)
 
             final_marked = cv2.flip(final_marked, 2)
             return (omr_response, final_marked)
@@ -475,8 +487,12 @@ class ImageInstanceOps:
                     3,
                 )
             for field_block_bubbles in field_block.traverse_bubbles:
-                for pt in field_block_bubbles:
-                    x, y = (pt.x + field_block.shift, pt.y) if shifted else (pt.x, pt.y)
+                for bubble_point in field_block_bubbles:
+                    x, y = (
+                        (bubble_point.x + field_block.shift, bubble_point.y)
+                        if shifted
+                        else (bubble_point.x, bubble_point.y)
+                    )
                     cv2.rectangle(
                         final_align,
                         (int(x + box_w / 10), int(y + box_h / 10)),
